cfb57eaa9adc6a439da432254fd0b47695de953a
sailorjacob <x2sides@gmail.com>
2025-09-29 12:42:47 -0400

    Increase footer background spacing to 80px
    - Changed translateY from 40px to 80px for more space below footer
    - Should now provide enough clearance for 'All rights reserved' text
    - Prevents any overlap with footer copyright content

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -36,7 +36,7 @@ export function Footer() {
         <div
           className="absolute bottom-0 left-0 right-0 z-0 m-0 p-0"
           style={{
-            transform: 'translateY(40px)', // Position below footer content
+            transform: 'translateY(80px)', // Position below footer content with more space
           }}
         >
           <div className="relative w-full h-32 overflow-hidden">
